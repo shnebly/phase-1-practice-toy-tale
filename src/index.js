642a6162ead77bf1f422844a622795afe5616657
Shelby Morris <shelbynicole.morris@gmail.com>
2023-03-11 18:11:32 -0700

    toy cards added, form submit listener successful POST to DOM and redirects to webpage

--- src/index.js
+++ src/index.js
@@ -12,4 +12,64 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+//Fetch Andy's Toys
+function getToys(){
+  fetch('http://localhost:3000/toys')
+      .then(resp => resp.json())
+      .then(data => {
+        for(let i = 0; i < data.length; i++){
+          showToy(data[i])
+        }
+      })
+      
+}
+getToys()
+// design the card for each toy
+function showToy(toy){
+  const collection = document.getElementById('toy-collection');
+  const card = document.createElement('div');
+  card.className = 'card';
+  const name = document.createElement('h2');
+  name.innerText = toy.name;
+  const img = document.createElement('img');
+  img.src = toy.image;
+  img.className = 'toy-avatar';
+  const likes = document.createElement('p');
+  likes.innerText = toy.likes;
+  const btn = document.createElement('button');
+  btn.className = 'like-btn';
+  btn.id = toy.id;
+  btn.innerText = 'Like';
+  card.append(name, img, likes, btn);
+  collection.append(card);
+}
+
+const form = document.querySelector(".add-toy-form")
+form.addEventListener('submit', createNewToy)
+
+function createNewToy(e){
+  e.preventDefault(e)
+  let newToy = {
+    name: e.target[0].value,
+    image: e.target[1].value,
+    likes: 0,
+  }
+  
+  const url = "http://localhost:3000/toys"
+  fetch(url, {
+    method:"POST",
+    headers:{
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newToy)
+  })
+  .then(resp => resp.json())
+  .then(data => console.log(data))
+}
+
+
+
+
+console.log("Last Load")
 });
