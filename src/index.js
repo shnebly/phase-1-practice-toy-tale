47e264d362c80269c66429590ffc044e8647b164
Shelby Morris <shelbynicole.morris@gmail.com>
2023-03-15 05:24:29 -0600

    completed tasks, unable to get DOM to reload without refresh

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ function showToy(toy){
   const collection = document.getElementById('toy-collection');
   const card = document.createElement('div');
   card.className = 'card';
+  card.id = toy.id;
   const name = document.createElement('h2');
   name.innerText = toy.name;
   const img = document.createElement('img');
@@ -39,34 +40,67 @@ function showToy(toy){
   likes.innerText = toy.likes;
   const btn = document.createElement('button');
   btn.className = 'like-btn';
-  btn.id = toy.id;
   btn.innerText = 'Like';
+
   card.append(name, img, likes, btn);
   collection.append(card);
+
+  card.querySelector('.like-btn').addEventListener('click', () => {
+    likes.innerText++
+    updateLike(card)
+  })
+
+  card.querySelector('h2').addEventListener('click', () => {
+    card.remove()
+    deleteCard(card)
+  })
 }
 
 const form = document.querySelector(".add-toy-form")
 form.addEventListener('submit', createNewToy)
 
 function createNewToy(e){
-  e.preventDefault(e)
+  e.preventDefault()
   let newToy = {
     name: e.target[0].value,
     image: e.target[1].value,
     likes: 0,
   }
-  
   const url = "http://localhost:3000/toys"
   fetch(url, {
     method:"POST",
     headers:{
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      // Accept: 'application/json'
     },
     body: JSON.stringify(newToy)
   })
   .then(resp => resp.json())
   .then(data => console.log(data))
+  form.reset() 
 }
+function updateLike(card){
+  fetch(`http://localhost:3000/toys/${card.id}`, {
+    method: 'PATCH',
+    headers:{
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(card)
+  })
+  .then(resp => resp.json())
+  .then(data => console.log(data))
+}
+function deleteCard(card){
+  fetch(`http://localhost:3000/toys/${card.id}`, {
+    method: 'DELETE',
+    headers:{
+      'Content-Type': 'application/json',
+    },
+  })
+  .then(resp => resp.json())
+  .then(data => console.log(data))
+}
+
 
 
 
